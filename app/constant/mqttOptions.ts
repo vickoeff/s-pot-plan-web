b8294f71ec1a6a5a63c2940b0ab12519792a34d8
gabypaulina <gabypaulina90@gmail.com>
2024-06-12 20:31:10 +0700

    feat: update logo and mqtt server

--- app/constant/mqttOptions.ts
+++ app/constant/mqttOptions.ts
@@ -1,4 +1,4 @@
-const BROKER_URL = 'ws://192.168.18.2:9001/mqtt';
-const OPTIONS = { clientId: 'nextjs_client' };
+const BROKER_URL = 'wss://919a078e0d7f4c6a93566fc5f7320b0d.s1.eu.hivemq.cloud:8884/mqtt';
+const OPTIONS = { username: 's-pot-web', password: "pg!6!C_x!PY5QFR" };
 
 export { BROKER_URL, OPTIONS };
